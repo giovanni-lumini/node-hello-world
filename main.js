00c6f57e5086b46bd798fb74b13ad1ec49e8b4b2
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-28 13:32:31 +0100

    start

---
+++ main.js
@@ -0,0 +1,11 @@
+/* 
+Creiamo la prima applicazione con NodeJs:
+Iniziate creando un file main.js con un semplice console.log che dica hello node js
+fate girare lo script usando node main.js
+aggiungete allo script una variabile di ambiente chiamata PIN  con associato numero a piacere es. PIN=666
+usate il modulo process per recuperare e stampare in console dalla proprietá env la variabile PIN
+ora passate un argomento al vostro script e recuperatene il valore usando process e la proprietá argv per stamparlo in console
+Bonus:
+se il valore dell'argomento é admin ed il valore di PIN definito é 666 allora stampa in console Welcome Admin
+altrimenti stampa Access restricted
+*/
